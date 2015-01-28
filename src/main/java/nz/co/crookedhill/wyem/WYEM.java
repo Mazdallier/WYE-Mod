0fa037566f120543a75770ba974d2e180930bb31
William Cameron <w.cameron@gmail.com>
2015-01-28 19:39:35 +1300

    default setup

---
+++ src/main/java/nz/co/crookedhill/wyem/WYEM.java
@@ -0,0 +1,18 @@
+package nz.co.crookedhill.wyem;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = WYEM.MODID, version = WYEM.VERSION)
+public class WYEM
+{
+    public static final String MODID = "wyem";
+    public static final String VERSION = "$VERSION$";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
