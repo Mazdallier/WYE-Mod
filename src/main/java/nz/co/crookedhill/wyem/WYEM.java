2c162f0be245e5741e81e0cc4ffaad0d674dd848
William Cameron <w.cameron@crookedhill.co.nz>
2015-01-29 18:06:58 +1300

    added onDamaged function, but doesnt work yet for some reason

--- src/main/java/nz/co/crookedhill/wyem/WYEM.java
+++ src/main/java/nz/co/crookedhill/wyem/WYEM.java
@@ -1,11 +1,18 @@
 package nz.co.crookedhill.wyem;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import nz.co.crookedhill.wyem.item.WYEMItem;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = WYEM.MODID, version = WYEM.VERSION)
 public class WYEM
@@ -20,5 +27,23 @@ public class WYEM
     public void init(FMLInitializationEvent event)
     {
 		WYEMItem.init();
+		MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @SubscribeEvent
+    public void onAttackedEvent(LivingAttackEvent event)
+    {
+    	/* if the attackee is a player and wearing the ender chestplate */
+    	if(event.entityLiving instanceof EntityPlayer && Arrays.asList(((EntityPlayer)event.entityLiving).inventory.armorInventory).contains(WYEMItem.enderChestplate) && event.source.isProjectile())
+    	{
+    		Random rand = new Random();
+    		//if(rand.nextFloat() <= 0.1f)
+    		//{
+    			event.setCanceled(true);
+    			event.entityLiving.posX = (event.entityLiving.posX-5d) + rand.nextDouble()*10d;
+    			event.entityLiving.posY = (event.entityLiving.posY-5d) + rand.nextDouble()*10d;
+    			event.entityLiving.posZ = (event.entityLiving.posZ-5d) + rand.nextDouble()*10d;
+    		//}
+    	}
     }
 }
