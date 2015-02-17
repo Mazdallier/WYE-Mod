eb7532419c7597290bd2c98b605a9cb688dd7296
William Cameron <w.cameron@gmail.com>
2015-02-17 13:45:13 +1300

    updated version

--- src/main/java/nz/co/crookedhill/wyem/WYEM.java
+++ src/main/java/nz/co/crookedhill/wyem/WYEM.java
@@ -24,7 +24,7 @@ import nz.co.crookedhill.wyem.proxy.ClientProxy;
 public class WYEM
 {
 	public static final String MODID = "wyem";
-	public static final String VERSION = "1.0.0.0";
+	public static final String VERSION = "1.0.0.1";
 	
 	@SidedProxy(clientSide="nz.co.crookedhill.wyem.proxy.ClientProxy", serverSide="nz.co.crookedhill.wyem.proxy.CommonProxy")
 	public static ClientProxy proxy;
